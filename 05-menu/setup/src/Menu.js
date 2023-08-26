327c51531e67d28d69dbb6b894839dc52335dbaf
László Czöndör <czondi@hotmail.com>
2023-08-26 19:38:28 +0200

    feat(project 5): implement menu

--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ items }) => {
+  return (
+    <div className="section-center">
+      {items.map(({ id, title, img, desc, price }) => (
+        <article className="menu-item" key={id}>
+          <img className="photo" src={img} alt={title} />
+          <div className="item-info">
+            <header>
+              <h4>{title}</h4>
+              <h4 className="price">${price}</h4>
+            </header>
+            <p className="item-text">{desc}</p>
+          </div>
+        </article>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;

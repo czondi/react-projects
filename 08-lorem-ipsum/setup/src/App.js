57d14be0124285a7b835af8ebda7499525a114e6
László Czöndör <czondi@hotmail.com>
2023-08-30 21:40:05 +0200

    feat(project 8): implement lorem ipsum

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,52 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const amount = parseInt(count, 10);
+    const quotient = Math.floor(amount / data.length);
+    const remainder = amount % data.length;
+
+    const paragpraphs = [
+      ...new Array(quotient).fill(data).flat(),
+      ...data.slice(0, remainder),
+    ];
+
+    setText(paragpraphs);
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>Tired of boring Lorem Ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraphs:</label>
+        <input
+          id="amount"
+          name="amount"
+          type="number"
+          min={1}
+          value={count}
+          onChange={(event) => setCount(event.target.value)}
+        />
+        <button type="submit" className="btn">
+          Generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <ol>
+          {text.map((paragraph, index) => (
+            <li key={index}>
+              <p>{paragraph}</p>
+            </li>
+          ))}
+        </ol>
+      </article>
+    </section>
+  );
 }
 
 export default App;

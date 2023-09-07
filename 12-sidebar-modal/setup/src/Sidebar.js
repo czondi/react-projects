14867048056559060d77ab5b94d30cd275af7c1b
László Czöndör <czondi@hotmail.com>
2023-09-07 21:00:15 +0200

    feat(project 12): implement sidebar modal

--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
 
-export default Sidebar
+  return (
+    <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <div className="sidebar-header">
+        <img className="logo" src={logo} alt="Coding addict" />
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map(({ id, url, text, icon }) => (
+          <li key={id}>
+            <a href={url}>
+              {icon}
+              {text}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <ul className="social-icons">
+        {social.map(({ id, url, icon }) => (
+          <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
